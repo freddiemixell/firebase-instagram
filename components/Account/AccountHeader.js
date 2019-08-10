12470568b6b657d5514b123557a318a17773d537
freddiemixell <fmixell@gmail.com>
2019-08-10 16:33:07 -0400

    adding bio

--- components/Account/AccountHeader.js
+++ components/Account/AccountHeader.js
@@ -4,27 +4,38 @@ import { View, Image, Text, Button, StyleSheet } from 'react-native';
 const style = StyleSheet.create({
     headerStyle: { justifyContent: 'space-around', flexDirection: 'row', alignItems: 'center', height: 150 },
     profilePicStyle: { width: 70, height: 70, borderRadius: 70/2 },
-    usernameStyle: { fontSize: 18 },
-    headerRightStyle: { backgroundColor: 'blue', flex: 1, height: '100%', justifyContent: 'center', alignItems: 'center' },
-    headerLeftStyle: { backgroundColor: 'red', flex: 1, height: '100%', justifyContent: 'center', alignItems: 'center' },
+    usernameTextStyle: { fontSize: 20 },
+    usernameSectionStyle: {flex: 1, width: '100%', justifyContent: 'flex-end'},
+    bioTextStyle: { lineHeight: 25 },
+    bioSectionStyle: { flex: 1, width: '100%', justifyContent: 'center' },
+    headerRightStyle: { flex: 1, height: '100%', justifyContent: 'center', alignItems: 'center' },
+    headerLeftStyle: { flex: 1, height: '100%', justifyContent: 'center', alignItems: 'center' },
 });
 
 function AccountHeader(props) {
-    const { accountInfo: { profilePictureUrl, username } } = props;
-    const { headerStyle, headerRightStyle, headerLeftStyle, profilePicStyle, usernameStyle } = style;
+    const { accountInfo: { profilePictureUrl, username, bio } } = props;
+
+    const { headerStyle, headerRightStyle, headerLeftStyle, profilePicStyle, usernameTextStyle, bioTextStyle, bioSectionStyle, usernameSectionStyle } = style;
     const uri = profilePictureUrl ? profilePictureUrl : 'https://scontent-lga3-1.cdninstagram.com/vp/d5c47042a03d0d01f7da53c8e023a1eb/5DCD0694/t51.2885-19/s320x320/66113339_606527399754110_6647810716449374208_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com';
     return (
         <View style={headerStyle} >
-            <View style={ headerLeftStyle }>
+            <View style={headerLeftStyle}>
                 <Image
                     style={profilePicStyle}
                     source={{ uri }}
                 />
             </View>
             <View style={headerRightStyle}>
-                <Text style={usernameStyle}>
-                    { username }
-                </Text>
+                <View style={usernameSectionStyle}>
+                    <Text style={usernameTextStyle}>
+                        { username }
+                    </Text>
+                </View>
+                <View style={bioSectionStyle} >
+                    <Text style={bioTextStyle}>
+                        { bio }
+                    </Text>
+                </View>
                 <View>
                     <Button
                         title='Edit Profile'
