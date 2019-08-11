0255ca44aa1a80bace1bbb70f0f1592d7d960231
freddiemixell <fmixell@gmail.com>
2019-08-10 20:17:12 -0400

    edit account screen

---
+++ screens/EditAccountScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import BaseScreen from '../components/BaseScreen';
+
+export default class EditAccountScreen extends Component {
+    render() {
+        return (
+            <BaseScreen>
+                <Text>
+                    Edit Account
+                </Text>
+            </BaseScreen>
+        );
+    }
+}
