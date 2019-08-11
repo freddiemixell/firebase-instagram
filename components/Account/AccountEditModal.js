41a8034f928164bdf47bf511f2cd9d16692c874d
freddiemixell <fmixell@gmail.com>
2019-08-11 12:40:50 -0400

    update user with the brain

--- components/Account/AccountEditModal.js
+++ components/Account/AccountEditModal.js
@@ -1,55 +1,94 @@
 import React, { Component } from 'react';
-import { Text, TextInput, Button, StyleSheet } from 'react-native';
+import { TextInput, Button, StyleSheet } from 'react-native';
 import BaseModal from '../BaseModal';
 import { Brain } from '../../Crainium';
 
 const style = StyleSheet.create({
     inputStyle: { padding: 10, width: '90%', height: 50, marginBottom: 10, marginRight: 'auto', marginLeft: 'auto', borderRadius: 6, borderColor: 'gray', borderWidth: 1, },
-    openBtnStyle: {},
 });
 
 export default class AccountEditModal extends Component {
     state = {
-        usernameUpdate: '',
-        loading: '',
+        username: '',
+        firstName: '',
+        lastName: '',
     }
 
-    updateUsernameAsync = async () => {
-        const { username } = this.state;
+    updateUserAsync = async () => {
+        const { username, firstName, lastName } = this.state;
 
-        if (username) {
-            return Brain.setAccountInfo({ user: Brain.uid, username, });
+        // Make an exact copy of state that we can filter.
+        let setAccount = { ...this.state };
+
+        // Only update what's filled out.
+        for (let key in setAccount) {
+            if( ! setAccount[key] ) {
+                delete setAccount[key];
+            }
         }
 
-        return false;
+        // Check if any of our values are set, if so update.
+        if ( username || firstName || lastName ) {
+            try {
+                const { status } = await Brain.setAccountInfo({ user: Brain.uid, ...setAccount });
+                if ( 'success' === status ) {
+                    console.log('SUCCESSFUL');
+                    this.setState({ username: '', firstName: '', lastName: '', });
+                    return this.props.toggleModal();
+                } else {
+                    return console.log('UNSUCCESSFUL');
+                }
+            } catch(error) {
+                return console.log(error);
+            }
+            
+        }
+
+        // Return to account if not updated.
+        return this.props.toggleModal();
     }
 
     render() {
-        const { username, } = this.state;
+        const { username, firstName, lastName } = this.state;
         const { modalVisible, toggle, } = this.props;
-        const { inputStyle, openBtnStyle } = style;
+        const { inputStyle } = style;
         return (
             <BaseModal
                 modalVisible={modalVisible}
                 transparent={false}
-                animationType="slide"
+                animationType="fade"
                 toggle={toggle}
                 title="Edit Account"
                 closeBtnText="Close"
-                openBtnText="Forgot Password"
-                openBtnStyle={openBtnStyle}
+                customOpenBtn={true}
             >
                 <TextInput
                     placeholder={'Username'}
                     style={inputStyle}
-                    onChangeText={newUsername => this.setState({username: newUsername})}
+                    onChangeText={input => this.setState({username: input})}
                     value={username}
+                    autoCapitalize="none"
+                    returnKey="done"
+                />
+                <TextInput
+                    placeholder='First Name'
+                    style={inputStyle}
+                    onChangeText={input => this.setState({firstName: input})}
+                    value={firstName}
+                    autoCapitalize="none"
+                    returnKey="done"
+                />
+                <TextInput
+                    placeholder='Last Name'
+                    style={inputStyle}
+                    onChangeText={input => this.setState({lastName: input})}
+                    value={lastName}
                     autoCapitalize="none"
                     returnKey="done"
                 />
                 <Button
                     title='Update'
-                    onPress={this.updateUsernameAsync}
+                    onPress={this.updateUserAsync}
                 />
             </BaseModal>
         );
